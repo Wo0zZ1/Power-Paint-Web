bb5576d5b8a6490feca0c846b32521e1a78ee645
Wo0zZ1 <Mper06@mail.ru>
2026-02-21 22:00:24 +0300

    fix: fix BASE_API_URL

--- src/shared/api/baseApiUrl.ts
+++ src/shared/api/baseApiUrl.ts
@@ -2,5 +2,5 @@ import { isServer } from "@tanstack/react-query";
 
 export const BASE_API_URL = () => {
   if (isServer) return process.env.NEXTAUTH_URL + "/api";
-  return "api";
+  return "/api";
 };
