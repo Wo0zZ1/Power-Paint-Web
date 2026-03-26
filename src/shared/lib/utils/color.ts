d2e4a75b5965d51f870f377deacdd6a049595d98
Wo0zZ1 <Mper06@mail.ru>
2026-03-26 12:55:02 +0300

    fix: fix cursor colors

--- src/shared/lib/utils/color.ts
+++ src/shared/lib/utils/color.ts
@@ -8,25 +8,26 @@ export const generateRandomHslColor = () =>
 export const generateRandomHexColor = () => hslToHex(generateRandomHslColor());
 
 export const getContrastingTextColor = (bgColor: string): string => {
-  const hslMatch = bgColor.match(/hsl\((\d+),\s*(\d+)%,\s*(\d+)%\)/);
+  const hexMatch = bgColor.match(hexColorRegex);
   const rgbMatch = bgColor.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
 
-  if (hslMatch) {
-    const l = parseInt(hslMatch[3]);
+  let hslColor: string = "";
 
-    return l > 50 ? "#000000" : "#ffffff";
+  if (hexMatch) {
+    hslColor = hexToHsl(bgColor);
+  } else if (rgbMatch) {
+    hslColor = rgbToHsl(bgColor);
+  } else {
+    return "#ffffff"; // fallback
   }
 
-  if (rgbMatch) {
-    const r = parseInt(rgbMatch[1]);
-    const g = parseInt(rgbMatch[2]);
-    const b = parseInt(rgbMatch[3]);
+  const hslMatch = hslColor.match(/hsl\((\d+),\s*(\d+)%,\s*(\d+)%\)/);
 
-    const yiq = (r * 299 + g * 587 + b * 114) / 1000;
-    return yiq >= 128 ? "#000000" : "#ffffff";
-  }
+  if (!hslMatch) return "#ffffff";
 
-  return "#ffffff";
+  const l = parseInt(hslMatch[3]);
+
+  return l > 50 ? "#000000" : "#ffffff";
 };
 
 // Convert colors Section
