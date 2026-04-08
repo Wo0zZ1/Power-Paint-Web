687e414a06bd0cd35fb9fde88c08880f2877b90b
Wo0zZ1 <Mper06@mail.ru>
2026-04-08 23:11:46 +0300

    chore: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
+  reactCompiler: false,
+  poweredByHeader: false,
   typedRoutes: false,
   experimental: {
     authInterrupts: true,
