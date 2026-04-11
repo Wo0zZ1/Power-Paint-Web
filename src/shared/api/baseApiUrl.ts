ff42184c6acbb4a4b12edf45fd479c5a6db851d7
Максим <Mper06@mail.ru>
2026-04-11 18:07:46 +0300

    fix: fix PORT env

--- src/shared/api/baseApiUrl.ts
+++ src/shared/api/baseApiUrl.ts
@@ -1,7 +1,7 @@
 import { isServer } from "@tanstack/react-query";
 
 export const GET_BASE_API_URL = () => {
-  if (isServer) return `http://127.0.0.1:${process.env.PORT}/api`;
+  if (isServer) return `http://127.0.0.1:${process.env.WEB_PORT || 3000}/api`;
 
   return "/api";
 };
