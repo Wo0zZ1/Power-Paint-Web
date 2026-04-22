7557cfd745036b05836b30cd2342876c91d6aa87
Wo0zZ1 <Mper06@mail.ru>
2026-04-22 21:15:49 +0300

    chore: add testing-library/user-event

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,7 @@
+import { TextEncoder, TextDecoder } from "util";
+
+// В Node.js 18 и выше TextEncoder и TextDecoder доступны глобально, но в jsdom их нужно импортировать и добавить в глобальный объект
+Object.assign(global, { TextDecoder, TextEncoder });
+
 // Импортируем расширенные матчеры для jest из @testing-library/jest-dom
-// Это позволяет писать: expect(element).toHaveTextContent(/react/i)
 import "@testing-library/jest-dom";
